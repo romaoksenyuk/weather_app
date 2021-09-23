fa5d70c559d8ce2050f73d58125b832b486ec0a2
romanoksenuk <roma.oksenuk@gmail.com>
2021-09-23 20:44:42 +0300

    add comments

--- weather/views.py
+++ weather/views.py
@@ -2,11 +2,18 @@ import requests
 from django.shortcuts import render
 
 def index(request):
+
+#Просто ключ для url
     appid = '2cc2e81d07f3347a238059f078af497d'
+
+#Ссилка на сайт з якого буде батись інфа про погоду
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
     city = 'Lutsk'
+
+#Змінна в яку заносяться дані в форматі json та перетворються в список
     res = requests.get(url.format(city)).json()
 
+#Записую в змінну потрібні поля
     city_info = {
         'city': city,
         'temp': res["main"] ["temp"],
