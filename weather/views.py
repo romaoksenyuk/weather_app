d0f98cdab1399f92fc47242a5e572b1304428dc0
romanoksenuk <roma.oksenuk@gmail.com>
2021-09-24 12:29:21 +0300

    add form

--- weather/views.py
+++ weather/views.py
@@ -1,26 +1,34 @@
 import requests
 from django.shortcuts import render
+from . models import City
+from .forms import CityForm
 
 def index(request):
 
-#Просто ключ для url
     appid = '2cc2e81d07f3347a238059f078af497d'
-
-#Ссилка на сайт з якого буде батись інфа про погоду
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city = 'Lutsk'
 
-#Змінна в яку заносяться дані в форматі json та перетворються в список
-    res = requests.get(url.format(city)).json()
+    if(request.method =='POST'):
+        form = CityForm(request.POST)
+        form.save()
 
+    form = CityForm()
+    cities = City.objects.all()
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
 #Записую в змінну потрібні поля
-    city_info = {
-        'city': city,
-        'temp': res["main"] ["temp"],
-        'temp_min': res["main"] ["temp_min"],
-        'temp_max': res["main"] ["temp_max"],
-        'icon': res["weather"] [0] ["icon"]
-    }
-    context = {'info': city_info}
+        city_info = {
+            'city': city.name,
+            'temp': res["main"] ["temp"],
+            'temp_min': res["main"] ["temp_min"],
+            'temp_max': res["main"] ["temp_max"],
+            'icon': res["weather"] [0] ["icon"]
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
 
     return render(request, 'index.html', context)
